aa1253be021ef44cce44f0d629a74ec28c14136c
xachary <179740385@qq.com>
2024-10-17 15:12:15 +0800

    chore: fix deploy

--- src/lib/components/GridDragResize/GridDragResizeItem.vue
+++ src/lib/components/GridDragResize/GridDragResizeItem.vue
@@ -44,11 +44,11 @@ type Emit = {
     (e: 'update:rows', val: number): void
     (e: 'update:columns', val: number): void
     (e: 'startDrag', val: StartDragEvent): void
-    (e: 'select'): void
+    (e: 'select', val?: any): void
     (e: 'startResize', val: StartResizeEvent): void
-    (e: 'remove'): void
+    (e: 'remove', val?: any): void
     (e: 'dropStart', val: { ele: HTMLElement | undefined, remove: () => void | undefined }): void
-    (e: 'dropEnd'): void
+    (e: 'dropEnd', val?: any): void
 }
 
 const emit = defineEmits<Emit>()
